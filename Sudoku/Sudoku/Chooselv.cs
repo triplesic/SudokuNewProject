de9fb3723761c3a6b0679dc6f73a647022cef0a8
torleng <torleng@gmail.com>
2011-08-19 11:59:00 +0700

    interface

---
+++ Sudoku/Sudoku/Chooselv.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku
+{
+    class Chooselv : LevelChooser
+    {
+        public int selectedlv(String txtlv, int dimension)
+        {
+
+         
+
+                if (txtlv.Equals("Easy"))
+                {
+                    return 50;
+
+                }
+                else if (txtlv.Equals("Normal"))
+                {
+                    return 40;
+
+                }
+                else if (txtlv.Equals("Hard"))
+                {
+                    return 30;
+
+                }
+                else
+                {
+                    return 0;
+                }
+            
+        }
+    }
+}
+
+        //    }else if(dimension==4){
+
+        //        if (txtlv.Equals("Easy"))
+        //        {
+        //            return  7;
+
+        //        }
+        //        else if (txtlv.Equals("Normal"))
+        //        {
+        //            return  6;
+
+        //        }
+        //        else if (txtlv.Equals("Hard"))
+        //        {
+        //            return  5;
+
+        //        }
+        //        else
+        //        {
+        //            return  0;
+        //        }
+        //    }else
+        //    {
+        //        return  0;
+        //    }
+        //}
---
+++ Sudoku/Sudoku/Chooselv9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku
+{
+    class Chooselv9 : LevelChooser
+    {
+        public int selectedlv(String txtlv)
+        {
+
+                if (txtlv.Equals("Easy"))
+                {
+                    return 50;
+
+                }
+                else if (txtlv.Equals("Normal"))
+                {
+                    return 40;
+
+                }
+                else if (txtlv.Equals("Hard"))
+                {
+                    return 30;
+
+                }
+                else
+                {
+                    return 0;
+                }
+            
+        }
+    }
+}
+
+     
